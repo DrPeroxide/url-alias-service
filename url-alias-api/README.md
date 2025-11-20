dfa8abab7d39a62102aba4c2af80d9c43d8e1366
Sean <biscuitsburnt@gmail.com>
2025-11-20 02:56:01 +0000

    Initialise API module - Add Maven POM generated via via Spring Boot - Add openapi generator plugin to create api interfaces - Add dockerfile and docker-compose configured with mongo database

---
+++ url-alias-api/README.md
@@ -0,0 +1,7 @@
+# URL Alias API
+
+Rest API handling server-side logic and data management for the URl Alias app.
+
+
+## OpenAPI Spec Additions
+- Added operationId used to generate API method names
---
+++ url-alias-api/src/main/java/dev/seanharris/urlalias/api/UrlAliasApiApplication.java
@@ -0,0 +1,17 @@
+package dev.seanharris.urlalias.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableConfigurationProperties
+public class UrlAliasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlAliasApiApplication.class, args);
+	}
+
+}
